0dcd9815498005b115a5a97077d19ce6c747b867
Nguyen Nguyen <hai_nguyen@ohmnilabs.com>
2023-05-21 17:04:05 +0700

    dev for import feature

--- controllers/import.controller.js
+++ controllers/import.controller.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const config = require('../configs/config');
 const utils = require('../helpers/utils');
+const { time } = require('console');
 
 const { data_path } = config;
 
@@ -10,38 +11,49 @@ module.exports = {
   importData: (req, res) => {
     const file = req.files.file;
     const fileName = file.name;
-    console.log(fileName);
-    //restore file in data folder
     async.waterfall([
       cb => file.mv(path.join(data_path, fileName), (err) => {
         cb(err)
       }),
-      // read file csv with csv to json, process data and send back to client
       cb => {
         utils.getCsvData(path.join(data_path, fileName)).then(csv_data => {
-          console.log(csv_data);
-            // // process data
-            // const dataArr = data.split('\n');
-            // const header = dataArr[0].split(',');
-            // const dataArrProcessed = dataArr.slice(1).map((item) => {
-            //   const itemArr = item.split(',');
-            //   const itemObj = {};
-            //   header.forEach((key, index) => {
-            //     itemObj[key] = itemArr[index];
-            //   });
-            //   return itemObj;
-            // });
-            cb(null);
-          })
-        }
-    ], (err) => {
+          let timeArr = [];
+          let tempArr = [];
+          let pressArr = [];
+          let flowArr = [];
+          let consumptionArr = [];
+          timeArr = csv_data.map(item => item.Insert_time.split(' ').splice(1, 4));
+          tempArr = csv_data.map(item => parseFloat(item.T));
+          pressArr = csv_data.map(item => parseFloat(item.P));
+          flowArr = csv_data.map(item => parseFloat(item.F));
+          consumptionArr = csv_data.map(item => parseFloat(item.kW));
+          let timeList = utils.handleTimeDataImport(timeArr);                                  //output: {timeArr: [...], dayArr: [...], weekArr: [...], monthArr: [...], day: {...}, week: {...}, month: {...}}
+          let flowList = utils.handleFlowDataImport(flowArr, timeList);                       //output: {day: [...], week: [...], month: [...]}
+          let consumptionList = utils.handleConsumptionDataImport(consumptionArr, timeList); //output: {day: [...], week: [...], month: [...]}
+          let data = {
+            time: timeList,
+            temp: tempArr,
+            press: pressArr,
+            flow: flowList,
+            consumption: consumptionList
+          }
+          cb(null, data);
+        })
+      }
+    ], (err, results) => {
       if (err) {
         console.log(err);
-        res.status(500).send({ message: 'Import failed', code: 200 });
-      } else {
-        console.log('Import success');
-        res.status(200).send({ success: true, code: 200 });
+        res.status(500).send({ success: false, message: 'Import failed', code: 200 });
+        return;
       }
+      console.log('Import success');
+      res.status(200).send({
+        success: true,
+        message: 'Import success',
+        data: results,
+        code: 200
+      });
+      return;
     });
   }
 }
--- helpers/utils.js
+++ helpers/utils.js
@@ -7,4 +7,147 @@ utils.getCsvData = async (filePath) => {
   return data;
 }
 
+utils.handleTimeDataImport = (timeArr) => {
+  const result = {};
+  const day = {};
+  const week = {};
+  const month = {};
+  const dayArr = [];
+  const weekArr = [];
+  const monthArr = [];
+  timeArr.forEach((item, index) => {
+    const dateTemp = `${item[0]} ${item[1]}, ${item[2]} ${item[3]}`;
+    const date = new Date(dateTemp);
+    const dayKey = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+    const weekKey = `${utils.getWeek(date)}/${date.getFullYear()}`
+    const monthKey = `${date.getMonth() + 1}/${date.getFullYear()}`;
+
+    if (!dayArr.includes(dayKey)) {
+      dayArr.push(dayKey);
+    };
+    if (!day[dayKey]) {
+      day[dayKey] = {
+        from: index,
+        to: index,
+      };
+    } else {
+      day[dayKey].to = index;
+    }
+
+    if (!week[weekKey]) {
+      week[weekKey] = {
+        from: index,
+        to: index,
+      };
+    } else {
+      week[weekKey].to = index;
+    }
+    if (!monthArr.includes(monthKey)) {
+      monthArr.push(monthKey);
+    };
+    if (!month[monthKey]) {
+      month[monthKey] = {
+        from: index,
+        to: index,
+      };
+    } else {
+      month[monthKey].to = index;
+    }
+  })
+
+  dayArr.forEach((item, index) => {
+    const dateTemp = item.split('/');
+    const date = new Date(`${dateTemp[2]}-${dateTemp[1]}-${dateTemp[0]}`);
+    if (index === 0 || date.getDay() === 1) {
+      weekArr.push(item);
+    }
+  })
+
+  result.timeArr = timeArr;
+  result.dayArr = dayArr;
+  result.weekArr = weekArr;
+  result.monthArr = monthArr;
+  result.day = day;
+  result.week = week;
+  result.month = month;
+  console.log("result", result);
+  return result;
+}
+
+utils.getWeekImport = (date) => {
+  const week = Math.ceil((date.getDay() + 1 + days) / 7);
+  return week;
+}
+
+utils.handleFlowDataImport = (flowArr, timeList) => {
+  let flowTemp = {};
+  Object.entries(timeList.day).forEach(([day, value]) => {
+    let sumFlowDay = 0;
+    for (let i = value.from; i <= value.to; i++) {
+      sumFlowDay += flowArr[i] * 5 / 60;
+    }
+    if (!flowTemp.day) {
+      flowTemp.day = [];
+    }
+    flowTemp.day.push(sumFlowDay);
+  });
+  Object.entries(timeList.week).forEach(([week, value]) => {
+    let sumFlowWeek = 0;
+    for (let i = value.from; i <= value.to; i++) {
+      sumFlowWeek += flowArr[i] * 5 / 60;
+    }
+    if (!flowTemp.week) {
+      flowTemp.week = [];
+    }
+    flowTemp.week.push(sumFlowWeek);
+  });
+  Object.entries(timeList.month).forEach(([month, value]) => {
+    let sumFlowMonth = 0;
+    for (let i = value.from; i <= value.to; i++) {
+      sumFlowMonth += flowArr[i] * 5 / 60;
+    }
+    if (!flowTemp.month) {
+      flowTemp.month = [];
+    }
+    flowTemp.month.push(sumFlowMonth);
+  });
+  console.log("flowTemp", flowTemp)
+  return flowTemp;
+}
+
+utils.handleConsumptionDataImport = (consumptionArr, timeList) => {
+  let consumptionTemp = {};
+  Object.entries(timeList.day).forEach(([day, value]) => {
+    let consumptionDay = consumptionArr[value.to];
+    if (!consumptionTemp.day) {
+      consumptionTemp.day = [];
+    }
+    consumptionTemp.day.push(consumptionDay);
+  });
+  Object.entries(timeList.week).forEach(([week, value]) => {
+    let consumptionWeek = consumptionArr[value.to];
+    if (!consumptionTemp.week) {
+      consumptionTemp.week = [];
+    }
+    consumptionTemp.week.push(consumptionWeek);
+  });
+  Object.entries(timeList.month).forEach(([month, value]) => {
+    let consumptionMonth = consumptionArr[value.to];
+    if (!consumptionTemp.month) {
+      consumptionTemp.month = [];
+    }
+    consumptionTemp.month.push(consumptionMonth);
+  });
+  console.log("consumptionTemp", consumptionTemp)
+  return consumptionTemp;
+}
+
+utils.getWeek = (date) => {
+  date.setHours(0, 0, 0, 0);
+  date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+  const week1 = new Date(date.getFullYear(), 0, 4);
+  return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
+                        - 3 + (week1.getDay() + 6) % 7) / 7);
+}
+
 module.exports = utils;
